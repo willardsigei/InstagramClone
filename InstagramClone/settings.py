fb42f54e1d20fa2295036647ce367cff48575af0
willardsigei <kipngeno.sigei@student.moringaschool.com>
2021-12-03 14:48:44 +0300

    created urls pattens

--- InstagramClone/settings.py
+++ InstagramClone/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Instagram',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,9 +76,10 @@ WSGI_APPLICATION = 'InstagramClone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'instagram',
+        'USER':'moringa',
+        'PASSWORD':'Access'    }
 }
 
 
