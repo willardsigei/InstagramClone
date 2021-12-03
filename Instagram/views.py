6dbdf51b525005877199e300afc1208d46bf5842
willardsigei <kipngeno.sigei@student.moringaschool.com>
2021-12-03 14:31:19 +0300

    created a vlogin view function.

--- Instagram/views.py
+++ Instagram/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Image, Profile,Comment,Followwww
+
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
+def index(request):
+    posts = Image.objects.all()
+    profile = Profile.objects.all()
+    comments = Comment.objects.all()
+    # profile = Profile.objects.filter(user=Image.profile.id).first()
+    # print('posts',posts)
+    return render(request,'index.html',{"posts":posts,"profile":profile,"comments":comments})
