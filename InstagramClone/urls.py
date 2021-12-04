7709989bd382d367b79f4a0ec95ab78ed84d40d1
willardsigei <kipngeno.sigei@student.moringaschool.com>
2021-12-04 12:44:58 +0300

    update dynamic urls

--- InstagramClone/urls.py
+++ InstagramClone/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.urls import path,include
 from django.contrib import admin
+from django.contrib.auth import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('',include('Instagram.urls')),
+
 ]
