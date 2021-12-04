b72b992a622dd51021f61a9ab4c4848424f7df88
willardsigei <kipngeno.sigei@student.moringaschool.com>
2021-12-04 12:31:39 +0300

    Added views classes and funtions

--- Instagram/views.py
+++ Instagram/views.py
@@ -10,6 +10,12 @@ from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+from django.views.generic import (
+    ListView,
+    DetailView,
+    DeleteView
+)
+
 # Create your views here.
 @login_required(login_url='/accounts/login/')
 def index(request):
@@ -67,3 +73,82 @@ def profile(request):
         }
 
     return render(request, 'profile.html', params)
+
+
+
+def prof(request):
+    # user_prof = get_object_or_404(User, username=username)
+    # if request.user == user_prof:
+    #     return redirect('profile', username=request.user.username)
+    profile = Profile.objects.filter(user = request.user)
+    return render(request,"users/profile.html",{"profile":profile})
+
+
+def editProfile(request):
+    if request.method == 'POST':
+
+        userForm = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = profileForm(
+            request.POST, request.FILES, instance=request.user)
+
+        if  profile_form.is_valid():
+            userForm.save()
+            profile_form.save()
+
+            return redirect('profile')
+
+    else:
+        
+        profile_form = profileForm(instance=request.user)
+        user_form = UserUpdateForm(instance=request.user)
+
+        params = {
+            'user_form':user_form,
+            'profile_form': profile_form
+
+        }
+
+    return render(request, 'editprofile.html', params)
+
+@login_required(login_url='/accounts/login/')
+def comment(request,id):
+    comments = Comment.objects.filter(postt= id)
+    images = Image.objects.filter(id=id).all()
+    current_user = request.user
+    user_profile = Profile.objects.get(user = current_user)
+    image = get_object_or_404(Image, id=id)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit = False)
+            comment.postt = image
+            comment.userr = user_profile
+            comment.save()
+            return HttpResponseRedirect(request.path_info)
+    else:
+        form = CommentForm()
+    return render(request,'comment.html',{"form":form,"images":images,"comments":comments})
+
+def searchprofile(request): 
+    if 'searchUser' in request.GET and request.GET['searchUser']:
+        name = request.GET.get("searchUser")
+        searchResults = Profile.search_profile(name)
+        message = f'name'
+        params = {
+            'results': searchResults,
+            'message': message
+        }
+        return render(request, 'results.html', params)
+    else:
+        message = "You haven't searched for any image category"
+    return render(request, 'results.html', {'message': message})
+
+class PostDetailView(DetailView):
+    model = Image
+    template_name= 'index.html'
+    def get_context_data(self, *args, **kwargs):
+        context=super(PostDetailView, self).get_context_data(*args, **kwargs)
+        stuff=get_object_or_404(Image, id=self.kwargs['id'])
+        total_likes=stuff.total_likes()
+        context["total_likes"]=total_likes
+        return context    
