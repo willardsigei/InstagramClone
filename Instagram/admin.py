c285210b568860d7120cf5c62c121596ca7b9d74
willardsigei <kipngeno.sigei@student.moringaschool.com>
2021-12-05 11:17:24 +0300

    added admin models

--- Instagram/admin.py
+++ Instagram/admin.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from .models import Image,Profile,Comment
+
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Image)
+admin.site.register(Profile)
+admin.site.register(Comment)
