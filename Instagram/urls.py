183aa316f5d995bc5a864df152dd68f10881d4e7
willardsigei <kipngeno.sigei@student.moringaschool.com>
2021-12-06 11:48:09 +0300

    debugging

--- Instagram/urls.py
+++ Instagram/urls.py
@@ -3,8 +3,11 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from django.contrib import admin
+
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('',views.index,name = 'home'),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('new/post/', views.newPost, name='newPost'),
